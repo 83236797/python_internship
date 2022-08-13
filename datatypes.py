9575fd48e462d3532f2f844200791344f03d1096
Vaishnavii <110148726+83236797@users.noreply.github.com>
2022-08-13 19:12:00 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+Name= "Pranali"
+string(Name)
+x = renge(10)
+print (type(x))
+dict = {"name" : "shiv", "age" : 25}
+print(type(dict))
+fruitSet= {"apple", "pear", "grapes"}
+print(type(fruitSet))
+fruitFrozenSet= ({"apple", "pear", "grapes"})
+print(type(fruitFrozenSet))
+boolValue = True
+print(type(boolValue)
+byteArray = bytearray(10)
+print(type(byteArray))
+NoneType=None
+print(type(None))
+tuple = ("apple", "pear", "grapes")
+print(type(tuple))
+fruitList=["apple", "pear", "grapes"]
+print(type(fruitlist))
+complexNumber = 10i
+print(type(complexNumber))
+floatNumber=10.5
+print(type(float))
+integerNumber=10
+print(type(integerNumber))
+
+
